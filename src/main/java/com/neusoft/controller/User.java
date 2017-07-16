ce3c1ff28e5f50fd1206e6d8e8d93a76d7a27ddf
lee_jiazh <lee_jiazh@163.com>
2017-07-16 10:45:58 +0800

    add mybatis configuration

--- src/main/java/com/neusoft/controller/User.java
+++ src/main/java/com/neusoft/controller/User.java
@@ -1,8 +1,18 @@
 package com.neusoft.controller;
 
+import com.neusoft.model.User1;
+import com.neusoft.mapper.IUserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by Bruce Lee on 2017/7/14.
@@ -16,4 +26,17 @@ public class User {
         mav.setViewName("index.jsp");
         return mav;
     }
+
+    @RequestMapping("test")
+    public ModelAndView test() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //创建会话工厂,传入Mybatis的配置文件信息
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
+        List<User1> users = userMapper.getSelectUser();
+        ModelAndView mav = new ModelAndView("index.jsp");
+        return mav;
+    }
+
 }
