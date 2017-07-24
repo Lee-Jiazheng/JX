47ec099039980256472c4cdc7a875324b482d769
ThornWu <wzzz00118@gmail.com>
2017-07-24 16:00:23 +0800

    2017-07-24

--- static/js/user_center.js
+++ static/js/user_center.js
@@ -4,11 +4,15 @@
                 $('#address_receiver_name').val('');
                 $('#address_contact').val('');
             });
-            $('.close_address_form').click(function(){
+            $('.close_form').click(function(){
                 $('.widgetcontainer').hide();
+                $('.icon_widgetcontainer').hide();
+                $('.password_widgetcontainer').hide();
             });
             $('.cancel_button').click(function(){
                 $('.widgetcontainer').hide();
+                $('.icon_widgetcontainer').hide();
+                $('.password_widgetcontainer').hide();
             });
             $('table tr .add_address').click(function(){
                 var row = $(this).parent().parent().index();
@@ -21,15 +25,42 @@
                 $('#address_contact').val(contact);
                 $('.widgetcontainer').show();
             });
+            
             $('.product_img').mouseover(function(){
-                $(this).animate({height:"165px"},550);
+                $(this).animate({height:"162px"},100);
             });
+            
             $('.product_img').mouseout(function(){
-                $(this).animate({height:"160px"},300);
+                $(this).animate({height:"160px"},100);
             });
+
             $('.favorite_item .product_info').mouseover(function(){
                 $(this).children(":first").show();
             });
             $('.favorite_item .product_info').mouseout(function(){
                 $(this).children(":first").hide();
+            });
+                        
+            var image_reg = new RegExp("^.*(jpg|jpeg|png)$");
+            $('.user_icon').click(function(){
+                $('.file_tip').text('点击此处上传图片');
+                $('.filename').html('&nbsp;');
+                $('.file_input').val('');
+                $('.icon_widgetcontainer').show();
+            });
+
+            $('.password_link').click(function(){
+                $('.password_widgetcontainer').show();
+            });
+
+            $('.file_input').change(function(){
+                if($(this).val().match(image_reg)){
+                    $('.filename').removeClass('alert');
+                    $('.file_tip').text('重新选择');
+                    $('.filename').text("您已选择："+$(this).val());
+                }
+                else{
+                    $('.filename').addClass('alert');
+                    $('.filename').text('上传失败');
+                }
             });
